dbf8eb11b34f735cb1b67d92576cf3e8646527cb
Tyler Holloway <tiloooor@Tylers-MacBook-Pro.local>
2019-12-05 13:35:45 -0500

    Capturing search input

--- src/components/Misc/SearchBar.js
+++ src/components/Misc/SearchBar.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+  const [formData, setFormData] = useState({
+    searchQuery: ''
+  });
+
+  const { searchQuery } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Submitting: ', searchQuery);
+  };
+
   return (
-    <div className="input-group-text mb-3">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Search..."
-        aria-describedby="basic-addon1"
-      ></input>
-    </div>
+    <form className="form" onSubmit={(e) => onSubmit(e)}>
+      <div className="form-group">
+        <div className="input-group-text mb-3">
+          <input
+            type="text"
+            class="form-control"
+            name="searchQuery"
+            placeholder="Search..."
+            onChange={(e) => onChange(e)}
+            aria-describedby="basic-addon1"
+          ></input>
+
+          <button type="submit" className="btn btn-general">
+            Search
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
