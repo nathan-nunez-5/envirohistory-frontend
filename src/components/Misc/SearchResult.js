473d096656348a30ea5443ec9fd39d6c9b2ed78b
Tyler Holloway <tiloooor@Tylers-MacBook-Pro.local>
2019-12-10 19:25:43 -0500

    Search result

--- src/components/Misc/SearchResult.js
+++ src/components/Misc/SearchResult.js
@@ -8,7 +8,7 @@ const searchResult = ({ result }) => {
         <h5 class="card-title">{result.title}</h5>
         <small>{result.year}</small>
         <p class="card-text">{result.snippet}</p>
-        <a href="/res" class="btn btn-primary">
+        <a href={result.link} class="btn btn-primary">
           Learn More
         </a>
       </div>
